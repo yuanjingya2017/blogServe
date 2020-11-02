4307bb3e494aa25e5a26299b9e59e3ffc002579f
yuanjingya <yuanjingya@czb365.com>
2020-11-02 13:47:02 +0800

    feat: blog后端代码

---
+++ app.js
@@ -0,0 +1,49 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+app.get("/news", function(req, res){
+  res.status = 200
+  let result = {
+    msg: 'get!',
+    arr: [1, 2, 3, 4]
+  }
+  res.send(result)
+  // req 请求对象，可以获取请求传递过来的信息
+  // res 响应对象，通过该对象可以控制响应的消息内容
+})
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+module.exports = app;
